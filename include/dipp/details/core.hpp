41ad893bebcfc18293f9b72a34e3d85742a6dd25
01Pollux <0139ne@gmail.com>
2025-01-10 16:17:26 +0100

    Add service invocation, Update unique/shared descriptor functor
    - Add 'invoke' method for auto injection in methods
    - Created a test suite in `services_invoke.cpp` to validate the `invoke` function's behavior.

--- include/dipp/details/core.hpp
+++ include/dipp/details/core.hpp
@@ -49,4 +49,56 @@ namespace dipp
             return "";
         }
     };
+
+    //
+
+    template<typename Ty>
+    concept function_descriptor_type = requires {
+        typename Ty::args_types;
+        typename Ty::return_type;
+    };
+
+    template<typename Ty> struct function_descriptor;
+
+    template<typename RetTy, typename... ArgsTy> struct function_descriptor<RetTy (*)(ArgsTy...)>
+    {
+        using args_types  = std::tuple<ArgsTy...>;
+        using return_type = RetTy;
+    };
+
+    template<typename RetTy, typename... ArgsTy> struct function_descriptor<RetTy(ArgsTy...)>
+    {
+        using args_types  = std::tuple<ArgsTy...>;
+        using return_type = RetTy;
+    };
+
+    template<typename RetTy, typename... ArgsTy> struct function_descriptor<std::function<RetTy(ArgsTy...)>>
+    {
+        using args_types  = std::tuple<ArgsTy...>;
+        using return_type = RetTy;
+    };
+
+    template<typename RetTy, typename... ArgsTy> struct function_descriptor<std::move_only_function<RetTy(ArgsTy...)>>
+    {
+        using args_types  = std::tuple<ArgsTy...>;
+        using return_type = RetTy;
+    };
+
+    template<typename FnTy> struct function_descriptor<std::function<FnTy>> : function_descriptor<FnTy>
+    {
+    };
+
+    //
+
+    template<function_descriptor_type FnTy, size_t Index>
+        requires(Index < std::tuple_size_v<typename FnTy::args_types>)
+    struct function_args_at
+    {
+        using type = std::tuple_element_t<Index, typename FnTy::args_types>;
+    };
+
+    template<function_descriptor_type FnTy> struct function_return_type
+    {
+        using type = typename FnTy::return_type;
+    };
 } // namespace dipp
