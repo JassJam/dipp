e38948172d485d2aea7d78b852187930a9987150
JassJam <0139ne@gmail.com>
2025-01-10 16:17:26 +0100

    Add service invocation, Update unique/shared descriptor functor
    - Add 'invoke' method for auto injection in methods
    - Created a test suite in `services_invoke.cpp` to validate the `invoke` function's behavior.

--- include/dipp/dipp.hpp
+++ include/dipp/dipp.hpp
@@ -4,3 +4,4 @@
 #include "details/collection.hpp"
 #include "details/provider.hpp"
 #include "details/injected.hpp"
+#include "details/invoke.hpp"
---
+++ tests/services_invoke/services_invoke.cpp
@@ -0,0 +1,59 @@
+#define BOOST_TEST_MODULE ServicesInvoke_Test
+
+#include <boost/test/included/unit_test.hpp>
+#include <dipp/dipp.hpp>
+#include <boost/compat/function_ref.hpp>
+
+BOOST_AUTO_TEST_SUITE(ServicesInvoke_Test)
+
+//
+
+struct Window
+{
+    int width;
+    int height;
+
+    Window(int width = 800, int height = 600) : width(width), height(height)
+    {
+    }
+};
+using WindowService = dipp::injected<Window, dipp::service_lifetime::singleton>;
+
+//
+
+void compare_window_equal(WindowService window, const Window& expected)
+{
+    BOOST_CHECK_EQUAL(window->width, expected.width);
+    BOOST_CHECK_EQUAL(window->height, expected.height);
+}
+
+void compare_window_not_equal(WindowService window, const Window& expected)
+{
+    BOOST_CHECK_NE(window->width, expected.width);
+    BOOST_CHECK_NE(window->height, expected.height);
+}
+
+//
+
+BOOST_AUTO_TEST_CASE(BasicInvoke)
+{
+    Window windowA(1024, 768);
+    Window windowB(1920, 1080);
+
+    //
+
+    dipp::default_service_collection collection;
+
+    collection.add<WindowService>([windowB](auto&) { return windowB; });
+
+    dipp::default_service_provider services(std::move(collection));
+
+    dipp::invoke(services, &compare_window_equal, windowB);
+    dipp::invoke(services, &compare_window_not_equal, windowA);
+    dipp::invoke(
+        services,
+        +[](WindowService window, const Window& expected) { BOOST_CHECK_EQUAL(window->width, expected.width); },
+        windowB);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
