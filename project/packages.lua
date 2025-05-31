65191e30fa85f14381add2ef2c0ab088a79bf9ed
01Pollux <0139ne@gmail.com>
2025-05-31 12:42:06 +0100

    Refactor package management by restructuring package installation functions and improving boost dependency handling

--- project/packages.lua
+++ project/packages.lua
@@ -1,17 +1,42 @@
-local boost_lib_version = "1.88.0"
-local boost_packages = {
-    "cmake",
-    "leaf"
-}
 
-if is_config("test", true) then
-    table.insert(boost_packages, "test")
+local function install_benchmark_packages()
+    if is_config("benchmark", true) then
+        add_requires("benchmark")
+        add_requires("kangaru")
+        add_requires("fruit")
+    end
 end
 
-add_requires("boost[".. table.concat(boost_packages, ",") .."] " .. boost_lib_version)
+local function install_result_packages()
+    local boost_lib_version = "1.88.0"
+    local boost_modules = {
+    }
 
-if is_config("benchmark", true) then
-    add_requires("benchmark")
-    add_requires("kangaru")
-    add_requires("fruit")
+    if is_config("test", true) then
+        table.insert(boost_modules, "test")
+    end
+    if is_config("error-type", "result") then
+        table.insert(boost_modules, "leaf")
+        table.insert(boost_modules, "regex")
+    end
+
+    -- if boost_modules is empty, then we don't need to install boost
+    if #boost_modules == 0 then
+        return
+    end
+
+    add_requires("boost[cmake,".. table.concat(boost_modules, ",") .."] " .. boost_lib_version)
+end
+
+install_result_packages()
+install_benchmark_packages()
+
+--
+
+function add_result_packages()
+    if not is_config("error-type", "result") then
+        return
+    end
+
+    add_packages("boost", {public = true})
 end
