b2d58c264cb2a7f502ca1bb2d35bf7f7d499dfa0
JassJam <0139ne@gmail.com>
2025-06-01 17:43:06 +0100

    Fix header file prefix directory for error headers in project.lua

--- project/project.lua
+++ project/project.lua
@@ -8,7 +8,7 @@ target("dipp")
 
     add_headerfiles(os.projectdir() .. "/include/dipp/*.hpp", {prefixdir = "dipp"})
     add_headerfiles(os.projectdir() .. "/include/dipp/details/*.hpp", {prefixdir = "dipp/details"})
-    add_headerfiles(os.projectdir() .. "/include/dipp/details/errors/*.hpp", {prefixdir = "dipp/details"})
+    add_headerfiles(os.projectdir() .. "/include/dipp/details/errors/*.hpp", {prefixdir = "dipp/details/errors"})
     add_includedirs(os.projectdir() .. "/include", {public = true})
 
     add_filegroups("dipp", {rootdir = os.projectdir()})
